3830b44ea9d2a24c2575019e388779e68e0c24c3
csm <csm009177@gmail.com>
2023-11-21 19:06:04 +0900

    5.js

--- app.js
+++ app.js
@@ -23,17 +23,29 @@
 // }
 
 // 4. '_pokemon' 배열에서 이름이 '리'로 시작하는 포켓몬만을 새로운 배열에 담아 출력해주세요.
-const _pokemon = ['피카츄', '파이리', '이상해씨'];
-let leeArray = [];
-for(i=0; i<_pokemon.length; i++){
-  if(_pokemon[i][0]==="피"){
-    leeArray = _pokemon[i]
-  }
-}
-console.log(leeArray)
-
-
+// const _pokemon = ['피카츄', '파이리', '이상해씨'];
+// let leeArray = [];
+// for(i=0; i<_pokemon.length; i++){
+  //   if(_pokemon[i][0]==="피"){
+    //     leeArray = _pokemon[i]
+    //   }
+    // }
+    // console.log(leeArray)
+    
 // 5. '_pokemon' 배열에서 특정 포켓몬이 있는지 확인하고 결과를 출력해주세요.
+const _pokemon = ['피카츄', '파이리', '이상해씨'];
+const selectPokemon = "피카"
+for(i=0; i<_pokemon.length; i++){
+  if(_pokemon[i]===selectPokemon){
+    console.log("exist")
+    return
+  } else{
+    console.log("no exist")
+    return
+  }
+}  
+
+
 // 6. '_pokemon' 배열의 원소들의 순서를 뒤집어 출력해주세요.
 // 7. '_pokemon' 배열의 원소들을 가나다 순으로 정렬해 출력해주세요.
 // 8. '_pokemon' 배열에서 총 6개의 포켓몬만을 새로운 배열에 담아 출력해주세요.
