6c40fa282d3f9dfed72f9320efdc1faf0a0e5284
csm <csm009177@gmail.com>
2023-11-21 18:02:29 +0900

    3.js

--- app.py
+++ app.py
@@ -15,18 +15,19 @@
 # print(pokemon)
 
 # 3. '_pokemon' 배열에서 찾고자 하는 포켓몬의 인덱스를 찾아주세요. 포켓몬이 배열에 없다면 -1을 출력하세요.
-pokemon = ['피카츄', '파이리', '이상해씨']
-wantPoke = '이상해씨'
-for i in range(len(pokemon)):
-  index=''
-  if pokemon[i] == wantPoke :
-    index = i
-    break
-print(index)
-
-
+# pokemon = ['피카츄', '파이리', '이상해씨']
+# wantPoke = '이상해씨'
+# for i in range(len(pokemon)):
+#   index=''
+#   if pokemon[i] == wantPoke :
+#     index = i
+#     break
+# print(index)
 
 # 4. '_pokemon' 배열에서 이름이 '리'로 시작하는 포켓몬만을 새로운 배열에 담아 출력해주세요.
+
+
+
 # 5. '_pokemon' 배열에서 특정 포켓몬이 있는지 확인하고 결과를 출력해주세요.
 # 6. '_pokemon' 배열의 원소들의 순서를 뒤집어 출력해주세요.
 # 7. '_pokemon' 배열의 원소들을 가나다 순으로 정렬해 출력해주세요.
