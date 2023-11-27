d925f24d0bb1a08a02a08cc6c9cb1ea86d7bfcf5
csm <csm009177@gmail.com>
2023-11-27 09:24:48 +0900

    9.py

--- app.py
+++ app.py
@@ -86,6 +86,13 @@
 
 
 # 9. '_pokemon' 배열과 다른 하나인 '_digimon' 배열을 연결해 새로운 배열을 생성해주세요.
+pokemon = ['피카츄', '라이츄', '파이리', '꼬부기', '버터풀', '야도란', '피죤투', '이상해씨' ]
+digimon = ['캬이츄', '츄이라', '리이파', '기부꼬']
+for i in range(len(pokemon)):
+        digimon = digimon + [pokemon[i]]
+print(digimon)
+
+
 # 10. '_pokemon' 배열의 길이(length 프로퍼티 사용제외)를 출력해주세요.
 # 11. '_pokemon' 배열에서 특정 인덱스의 포켓몬 이름을 새로운 이름으로 변경해주세요.
 # 12. '_pokemon' 배열의 모든 포켓몬 이름 뒤에 'Lv2'를 붙여 새로운 배열을 만들어 출력해주세요.
