e43635a653122f5ef5606a2e2216dff45f3a3831
csm <csm009177@gmail.com>
2023-11-29 09:36:38 +0900

    app.js 12

--- app.js
+++ app.js
@@ -98,16 +98,16 @@
 // console.log(count);
 
 // 11. '_pokemon' 배열에서 특정 인덱스의 포켓몬 이름을 새로운 이름으로 변경해주세요.
-const pokemon = ['피카츄', '라이츄', '파이리', '꼬부기', '버터풀', '야도란', '피죤투', '이상해씨'];
-function changeName(i, name){
-  for(i in pokemon){
-    if (pokemon[i]<pokemon){
-      pokemon[i] = name
-    }
-  }
-}
-changeName(0, "깨꾸리")
-console.log(pokemon)
+// const pokemon = ['피카츄', '라이츄', '파이리', '꼬부기', '버터풀', '야도란', '피죤투', '이상해씨'];
+// function changeName(i, name){
+//   for(i in pokemon){
+//     if (pokemon[i]<pokemon){
+//       pokemon[i] = name
+//     }
+//   }
+// }
+// changeName(0, "깨꾸리")
+// console.log(pokemon)
 
 // 12. '_pokemon' 배열의 모든 포켓몬 이름 뒤에 'Lv2'를 붙여 새로운 배열을 만들어 출력해주세요.
 // 13. '_pokemon' 배열의 모든 원소를 순회하며 출력해주세요.
